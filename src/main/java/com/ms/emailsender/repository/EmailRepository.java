f8f249420fb2023eb18b04d19da0fe1db8362f39
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-28 00:04:08 -0300

    implemented email repository

---
+++ src/main/java/com/ms/emailsender/repository/EmailRepository.java
@@ -0,0 +1,9 @@
+package com.ms.emailsender.repository;
+
+import com.ms.emailsender.model.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
