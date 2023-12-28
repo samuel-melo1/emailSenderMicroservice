109343c70b20c64c9fce90aea447c2510c112863
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-28 00:03:41 -0300

    implemented send email

--- src/main/java/com/ms/emailsender/consumers/EmailConsumer.java
+++ src/main/java/com/ms/emailsender/consumers/EmailConsumer.java
@@ -1,15 +1,26 @@
 package com.ms.emailsender.consumers;
 
 import com.ms.emailsender.dtos.EmailRecordDto;
+import com.ms.emailsender.model.EmailModel;
+import com.ms.emailsender.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    private EmailService emailService;
+
+    public EmailConsumer(EmailService emailService){
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues =  "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
