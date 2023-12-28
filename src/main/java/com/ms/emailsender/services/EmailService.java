4673321f667042ddb84634261916d92becf6c84a
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-12-28 00:04:23 -0300

    implemented  service email

---
+++ src/main/java/com/ms/emailsender/services/EmailService.java
@@ -0,0 +1,48 @@
+package com.ms.emailsender.services;
+
+import com.ms.emailsender.enums.StatusEmail;
+import com.ms.emailsender.model.EmailModel;
+import com.ms.emailsender.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+
+    private EmailRepository emailRepository;
+    private JavaMailSender emailSender;
+
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
+        this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+        try{
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException exception){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+            return emailRepository.save(emailModel);
+        }
+    }
+}
